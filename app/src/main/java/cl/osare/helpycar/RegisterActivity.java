85c29dcbc70526f3adeb6cb7c3e32722af8f9f19
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2015-11-18 12:48:32 -0300

    Update v3.0
    - Fixing dependencies

---
+++ app/src/main/java/cl/osare/helpycar/RegisterActivity.java
@@ -0,0 +1,14 @@
+package cl.osare.helpycar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RegisterActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
